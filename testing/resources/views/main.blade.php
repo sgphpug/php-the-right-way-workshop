070af1d5715d688491e145a5b9bfb7aa828ade63
Hanif Norman <hanif.norman@2359media.com>
2016-08-12 15:46:52 +0800

    Initial setup for testing project

---
+++ testing/resources/views/main.blade.php
@@ -0,0 +1,14 @@
+<!-- resources/views/main.blade.php -->
+
+<html>
+<head>
+    <title>Motivaitonal — Your daily source of motivation!</title>
+    <link href="/css/style.css" rel="stylesheet" type="text/css"/>
+    <link href='http://fonts.googleapis.com/css?family=Alegreya:400,700|Roboto+Condensed' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="container">
+    <h1>Testing</h1>
+</div>
+</body>
+</html>
